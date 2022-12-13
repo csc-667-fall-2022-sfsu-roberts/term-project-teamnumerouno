0018cbab82b6a131984ce7d2bec821fa8a299fc7
jzaionz@mail.sfsu.edu <jzaionz@mail.sfsu.edu>
2022-12-12 17:28:00 -0800

    Front-end work that hopefully will at least work okay.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const sessionInstance = require() 
+const protect = require();
 
 // To use .env
 if(process.env.NODE_ENV === 'development') {
